8d954908148e6a9f36c6e9379a4c14cb31aa6139
keno <keno.ss57@gmail.com>
2020-12-02 23:19:39 +0900

    Add test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn message() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", message());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_message() {
+        assert_eq!(message(), "Hello, world!");
+    }
 }
